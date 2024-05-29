0e83e85afd0f715274ef470f48274c6116f7acc0
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-05-29 22:25:29 +0530

    fix: footer socials padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex max-md:gap-6 mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Aaqib Javaid
         </p>
