3f2bb0aec0bb1357106467e92144a670642bc071
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-05-29 21:53:40 +0530

    feat: initial commmit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Aaqib was an exceptionally professional web developer. He built a website for a business that deals with many complex international regulations and was able to deliver an elegant website. He guided me through the process and as someone who is not at all proficient with web design/development it was very helpful! He was understanding of all individual concerns and issues I had to overcome for my businesses website and created a beautiful custom build!!",
+    name: "James Mason",
+    title: "Freelance Client",
+  },
+  {
+    quote:
+      "The project was completed in a timely manner with clear, concise and quick communication, leading to a very thoughtful and professional programming solution. I had never used fiverr before to outsource programmers and I would 100% return, especially to this provider, for further software solution development.",
+    name: "Juan Kuhn",
+    title: "Freelance Client",
+  },
+  {
+    quote:
+      "Very good communication , did a good job , he was able to program and fix website with out source code which I could not furnish . Will be using him for my second round of changes to the website .",
+    name: "Robert Leon",
+    title: "Freelance Client",
+  },
+  {
+    quote:
+      "Aaqib helped me with a tricky issue integrating a third party service on my front end, and was highly communicative and collaborative throughout, would gladly hire him again!",
+    name: "Rogue Botanist",
+    title: "Freelance Client",
+  },
+  {
+    quote:
+      "I reached out to Aaqib for a project that I have. We had a discussion on what was required and how he could assist with the task. He identified a time line and we proceeded with the work agreement. During the work he provided updates and answered my question. He was able to provide a screenshot of changes. The final product was delivered with instructions and support. I am satisfied with the results.",
+    name: "Shayna Grace",
+    title: "Freelance Client",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Fullstack Instructor - Testbook",
+    desc: "Created and delivered engaging course content, guided students through hands-on projects, and provided support to help them master both front-end and back-end technologies.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Developer - TPMS",
+    desc: "Designed and implementented user interfaces for TPMS web application. Worked closely with the design and backend teams to create seamless and visually appealing user experiences.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Fullstack Freelance Developer - Upwork/Fiverr",
+    desc: "I offer comprehensive web development services, handling both front-end and back-end tasks. I collaborate with clients to create dynamic websites and applications, utilizing my skills in various programming languages and frameworks to deliver high-quality, customized solutions.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/aaqib605",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://twitter.com/aaqib605",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/aaqib605/",
+  },
+];
